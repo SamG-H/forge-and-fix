a9a1cea7514e7e8d743454d457fbba58b5f08e45
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-11 15:53:28 -0700

    add signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    session.clear
+    redirect_to signin_path
   end
 end
