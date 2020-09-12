8710109a23e7a51f5372e2ffca979ca28372b4cd
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-12 16:59:26 -0700

    utilize many-to-many relationship task

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,5 +2,6 @@ class Issue < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   belongs_to :project
+  has_many :tasks
   has_many :users, through: :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :username, presence: true
   has_many :projects
+  has_many :tasks
   has_many :issues, through: :tasks
 end
