0e3ec84e8627e557096f3ae3b212ca489815324e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-14 11:32:34 -0700

    issues and tasks destroyed when project is

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,6 @@ class Issue < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   belongs_to :project
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_many :users, through: :tasks
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :category, presence: true
   belongs_to :user
-  has_many :issues
+  has_many :issues, dependent: :destroy
 
   scope :list_category, ->(category) { where("category == ?", category) }
 end
