02af57d7df9de2cd5d2b2d2f9074d16319be28c7
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-18 13:15:22 -0700

    organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  match '/auth/:provider/callback', to: 'sessions#github', via: [:get, :post]
-  get '/issues/:id/tasks/new', to: 'tasks#new', as: :new_task
-  post '/issues/:id/tasks', to: 'tasks#create', as: :create_task
-  patch '/issues/:id/tasks', to: 'tasks#update', as: :update_task
-  resources :tasks, only: [:index, :show, :edit, :update, :destroy]
   root 'static#welcome'
-  resources :projects do
-    resources :issues
-  end
+  match '/auth/:provider/callback', to: 'sessions#github', via: [:get, :post]
   resources :users, only: [:create, :show]
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :projects do
+    resources :issues
+  end
+
+  resources :tasks, only: [:index, :show, :edit, :update, :destroy]
+  get '/issues/:id/tasks/new', to: 'tasks#new', as: :new_task
+  post '/issues/:id/tasks', to: 'tasks#create', as: :create_task
+  patch '/issues/:id/tasks', to: 'tasks#update', as: :update_task
 end
