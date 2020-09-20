ed45b0e0c64a68190e9e50d115873292e7c56677
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-20 16:25:50 -0700

    refactor oath class function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,10 @@ class User < ApplicationRecord
   has_many :issues, through: :tasks
 
   def self.find_or_create_with_oauth(auth)
-    user = User.find_by_uid(auth["uid"])
-    return user if user
-    user = User.new
-    user.username = auth["extra"]["raw_info"]["login"]
-    user.uid = auth["uid"]
-    user.password = SecureRandom.hex(20)
-    user.save
-    user
+    user = User.find_or_create_by(uid: auth["uid"]) do |u|
+      u.username = auth["extra"]["raw_info"]["login"]
+      u.password = SecureRandom.hex(20)
+    end
   end
   
 end
