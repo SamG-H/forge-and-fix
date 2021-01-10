7bee589e52c7f6124bdeee7c7f9d6e95885b36d8
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-01-10 15:58:10 -0800

    add dependent destroy for comments

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,5 +4,5 @@ class Issue < ApplicationRecord
   belongs_to :project
   has_many :tasks, dependent: :destroy
   has_many :users, through: :tasks
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
