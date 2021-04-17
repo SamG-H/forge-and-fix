1228df98d95596ec95bf58e0e6683cd24d4e63ad
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-04-17 14:17:53 -0700

    fix indentation

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :issues, dependent: :destroy
 
-  scope :list_category, ->(category_search) { where("category == ?", category_search) }
+  scope :list_category, -> (category_search) { where("category LIKE ?", category_search) }
   def self.order_by_issue_count
     Project.left_joins(:issues).group(:id).order('COUNT(issues.id) DESC')
   end
