71a7f75619af1b60165c5a88d523ab5e31a5c93d
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-04-17 20:13:19 -0700

    migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_28_033855) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "issue_id"
